75dfad3d3ae6b93573f303acc2e36f3d8d9caf7d
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-29 15:11:03 +0000

    Filled out Interaction testing

--- src/InboxScreen.stories.jsx
+++ src/InboxScreen.stories.jsx
@@ -58,3 +58,39 @@ export const PinTask = {
     await expect(unpinButton).toBeInTheDocument();
   },
 };
+
+export const ArchiveTask = {
+  parameters: {
+    ...Default.parameters,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const getTask = (id) => canvas.findByRole("listitem", { name: id });
+    const itemToArchive = await getTask("task-2");
+    // Find the archive button
+
+    const archiveButton = await findByRole(itemToArchive, "button", {
+      name: "archiveButton-2",
+    });
+
+    // Click the archive button
+    await userEvent.click(archiveButton);
+  },
+};
+
+export const EditTask = {
+  parameters: {
+    ...Default.parameters,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const getTask = (id) => canvas.findByRole("listitem", { name: id });
+
+    const itemToEdit = await getTask("task-5");
+    const taskInput = await findByRole(itemToEdit, "textbox");
+    await userEvent.type(taskInput, " and disabled state");
+    await expect(taskInput.value).toBe(
+      "Fix bug in input error state and disabled state"
+    );
+  },
+};
